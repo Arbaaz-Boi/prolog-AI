e5a0bfcbdd4c2454b88d605ffe4c93adf2b975ee
Arbaaz-Boi <127650728+Arbaaz-Boi@users.noreply.github.com>
2023-03-12 00:17:47 -0800

    create lab file

---
+++ lab.pl
@@ -0,0 +1,23 @@
+male(arbaaz).
+male(huzaifa).
+male(qaseem).
+male(omais).
+male(arsalan).
+
+%REALTIONSHIP.
+father(arsalan,saima).
+father(sharukh, ali).
+father(sharukh,abid).
+father(sharukh,jhon).
+friend(ali,farhan).
+drive(farhan,car).
+likes(ali,icecream).
+
+%properties.
+women(saima).
+man(farhan).
+walk(farhan).
+happy(ali).
+
+
+
